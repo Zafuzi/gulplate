aa85b037b7a99e0a1d797a82045d8cd9caad2e09
Zachary Foutz <zacharyfoutz@gmail.com>
2017-06-16 22:26:41 -0700

    Initial Changes: Move to current GULP template

--- gulpfile.js
+++ gulpfile.js
@@ -2,23 +2,61 @@ var gulp = require('gulp'),
     pug = require('gulp-pug'),
     less = require('gulp-less'),
     minifyCSS = require('gulp-csso'),
-    watch = require('gulp-watch');
+    watch = require('gulp-watch'),
+    gp_rename = require('gulp-rename'),
+    gp_concat = require('gulp-concat'),
+    gp_uglify = require('gulp-uglify'),
+    cleanCSS = require('gulp-clean-css');
+
+
+var vendor = {
+    "js": ["bower_components/modernizer/modernizr.js", "bower_components/onscrolling/dist/onscrolling.js", "bower_components/smooth-scroll/smooth-scroll.js"],
+    "css": ["bower_components/typicons.font/src/font/typicons.min.css", "bower_components/normalize-css/normalize.css"]
+}
+
+gulp.task("minifyvendor", function() {
+    gulp.src(vendor.js)
+        .pipe(gp_concat('vendor.js'))
+        .pipe(gulp.dest('.'))
+        .pipe(gp_rename('vendor.min.js'))
+        .pipe(gp_uglify())
+        .pipe(gulp.dest('.'));
+
+    gulp.src(vendor.css)
+        .pipe(cleanCSS())
+        .pipe(gulp.dest("production/css"))
+});
 
 gulp.task('html', function() {
-    return watch('build/templates/*.pug', function() {
-        gulp.src('build/templates/*.pug')
+    return watch('./build/templates/*.pug', function() {
+        gulp.src('./build/templates/*.pug')
             .pipe(pug())
-            .pipe(gulp.dest('production/html'))
+            .pipe(gulp.dest('production/html'));
     });
 });
 
 gulp.task('css', function() {
-    return watch('build/styles/*.less', function() {
-        gulp.src('build/styles/*.less')
+    return watch('./build/styles/prefixed/*.less', function() {
+        gulp.src('./build/styles/prefixed/*.less')
             .pipe(less())
             .pipe(minifyCSS())
-            .pipe(gulp.dest('production/css'))
+            .pipe(gulp.dest('production/css'));
     });
 });
 
-gulp.task('default', ['html', 'css']);
+// Auto prefixer for converting css to work on older browsers
+gulp.task('autoprefixer', function() {
+    var postcss = require('gulp-postcss');
+    var sourcemaps = require('gulp-sourcemaps');
+    var autoprefixer = require('autoprefixer');
+
+    return watch('./build/styles/*.less', function() {
+        gulp.src('./build/styles/*.less')
+            .pipe(sourcemaps.init())
+            .pipe(postcss([autoprefixer()]))
+            .pipe(sourcemaps.write('./maps/'))
+            .pipe(gulp.dest('./build/styles/prefixed/'));
+    });
+});
+
+gulp.task('default', ['html', 'autoprefixer', 'css', "minifyvendor"]);
